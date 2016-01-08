da7d5e40c68fc1ea90af3457e7fde3bb0ed0d61b
Alex Berardi <AlexMBerardi@gmail.com>
2016-01-08 17:54:05 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<script src="js/jquery.js"></script>
+	
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	
+	<?php include("weather.php")?>
+	<?php include("messages.php")?>
+	<?php include("drivetime.php")?>
+	<?php include("time.php")?>
+	
+
+	<script src="js/getLocation.js"></script>
+</body>
+</html>
