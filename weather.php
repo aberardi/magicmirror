5a5f4ebe479c8e5e83f0ca42885519ea7707b598
Alex Berardi <AlexMBerardi@gmail.com>
2016-03-20 14:32:26 -0400

    Added more compliments and switched weather to use openweathermap instead of yahoo due to consistency issues with yahoos api.

--- weather.php
+++ weather.php
@@ -5,34 +5,49 @@
 		<script>
 		$(document).ready(function() {
         getWeather();
-        setInterval(getWeather, 1800);
+        setInterval(getWeather, 600000);
     });
 
     function getWeather() {
-
-        navigator.geolocation.getCurrentPosition(function(position) {
-
-            var location = (position.coords.latitude + ',' + position.coords.longitude);
-            var woeid = '';
-
-            $.simpleWeather({
-                location: location,
-                woeid: woeid,
-                unit: 'f',
-                success: function(weather) {
-                    html = '<h2>' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
-                    html += '<ul><li>' + weather.city + ', ' + weather.region + '</li>';
-                    html += '<li class="currently">' + weather.currently + '</li></ul>';
-                    html += '<li>Tomorrow\'s forecast: <br/>' + weather.forecast[1].code + '&deg;' + weather.units.temp + '<br/>' + weather.forecast[1].text + '</li></ul>';
-
-                    $("#weather").html(html);
-                },
-                error: function(error) {
-                    $("#weather").html('<p>' + error + '</p>');
-                }
-            });
-        });
+        var weather = 'http://api.openweathermap.org/data/2.5/weather?zip=08724,us&units=imperial&appid=6d3cc3a632aaa6c93edd758ea9f36de9';
+    $.ajax({
+      data: {
+      format: 'json'
+      },
+      dataType: "jsonp",
+      url: weather,
+      success: function(data)
+      {
+        html = '<h2>' + Math.round(data.main.temp) + '&deg;F </h2>';
+        html += '<ul><li> Brick, NJ</li></ul><ul><li>' + data.weather[0].description + '</li><li>High:' + Math.round(data.main.temp_max) + '&deg;F</li><li>Low:' + Math.round(data.main.temp_min) + '&deg;F</li></ul>';
+      
+        $("#weather").html(html);
+        getForecast();
+      }
+    });
+    
     }
+    function getForecast()
+    {
+        var forecastURL = 'http://api.openweathermap.org/data/2.5/forecast/daily?zip=08724,us&units=imperial&appid=6d3cc3a632aaa6c93edd758ea9f36de9'
+        //var forecast += '<li>Tomorrow\'s forecast: <br/>' + weather.forecast[1].code + '&deg;' + weather.units.temp + '<br/>' + weather.forecast[1].text + '</li></ul>';  
+         $.ajax({
+      data: {
+      format: 'json'
+      },
+      dataType: "jsonp",
+      url: forecastURL,
+      success: function(data)
+      {
+        var forecast = $("#weather").html() + '<li>Tomorrow\'s forecast:</br>' + data.list[1].weather[0].description + ' ' + Math.round(data.list[1].temp.day) + '&deg;F';
+
+        $("#weather").html(forecast);
+      }
+
+    });
+     }
+
+
     </script>
 	<div id="weather-panel">
 		<div id="weather"></div>
